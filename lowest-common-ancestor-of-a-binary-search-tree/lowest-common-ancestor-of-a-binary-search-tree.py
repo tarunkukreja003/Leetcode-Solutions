a709930b729a3483772b118ba33bddbcd3348aae
tarunkukreja003 <tkukreja@horizon.csueastbay.edu>
2021-12-16 21:23:30 -0800

    Time: 76 ms (76.46%), Space: 18.3 MB (46.05%) - LeetHub

--- lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
+++ lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,14 +7,34 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        def dfsRecursiveLCA(currNode):
+        # can p and q be the same? - no
+        # since its a BST - any 2 elements can't be same right? - correct
+        # if p is a parent of q or vica-versa then parent will be returned
+        
+        
+        # if p.val > q.val then p can be the ancestor or the ancestro is between them
+        # write the code to find the LCA between the 2
+        
+        # if the curr Node is in between p and q or equal to either then thats the LCA
+        def recursiveFindLCA(currNode, p, q):
+            if currNode == None:
+                return
             
-            if (p.val <= currNode.val and currNode.val <= q.val) or (q.val <= currNode.val and currNode.val <= p.val):
+            
+            if (currNode.val >= q.val and currNode.val <= p.val) or (currNode.val <= q.val and currNode.val >= p.val):
+                    # currNode is the ancestor
                 return currNode
-            elif (p.val < currNode.val and q.val < currNode.val):
-                return dfsRecursiveLCA(currNode.left)
             
-            elif (p.val > currNode.val and q.val > currNode.val):
-                return dfsRecursiveLCA(currNode.right)
+            elif p.val > currNode.val and q.val > currNode.val:
+                return recursiveFindLCA(currNode.right, p, q) 
             
-        return dfsRecursiveLCA(root)
+            elif p.val < currNode.val and q.val < currNode.val:
+                return recursiveFindLCA(currNode.left, p, q) 
+            
+            
+            
+            
+        
+        return recursiveFindLCA(root, p, q)
+            
+        
