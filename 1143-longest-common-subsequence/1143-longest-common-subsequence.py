0f8747c68d2f402805b8d984ab2896dc5f2d55c7
tarunkukreja003 <tkukreja@horizon.csueastbay.edu>
2022-02-05 15:45:27 -0800

    Time: 558 ms (54.17%), Space: 22.7 MB (54.27%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -34,17 +34,41 @@ class Solution:
         
         """
         
+        # bottom-up 
         
-        @lru_cache(maxsize=None)
-        def findLCS(i, j):
-            if i == len(text1) or j == len(text2):
-                return 0
-            if text1[i] == text2[j]:
-                # move both the pointers
-                return findLCS(i+1, j+1) + 1 
-            
-            else:
-                # move either pointer
-                return max(findLCS(i+1, j), findLCS(i, j+1))        
+        # initialise dp array, here there are 2 states, hence 2d array. Since we are finding the max length of common subsequence, length cannot be negative hence we set default values to 0
         
-        return findLCS(0,0)
+        lenText1 = len(text1)
+        lenText2 = len(text2)
+        dp = [[0 for _ in range(lenText2+1)] for _ in range(lenText1+1)]
+        
+        # set the base case and return value
+        
+        # default val -> if we reach lenText2 or lenText1 then its 0
+        
+        for i in range(lenText1):
+            dp[i][lenText2] = 0
+        
+        for j in range(lenText2):
+            dp[lenText1][j] = 0
+        
+        # we'll use nested for loops 
+        
+        # we'll be traversing backwards
+        
+        for i in range(lenText1-1,-1,-1):
+            for j in range(lenText2-1,-1,-1):
+                if text1[i] == text2[j]:
+                    dp[i][j] = dp[i+1][j+1] + 1
+                    
+                else:
+                    dp[i][j] = max(dp[i+1][j], dp[i][j+1])
+        
+        
+                    
+                    
+                
+        
+        
+        return dp[0][0]
+        
