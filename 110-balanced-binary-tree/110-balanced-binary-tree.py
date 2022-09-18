c7f4934f3bb7efbf5cdac37ec4201b1fd6caf1dd
Tarun Kukreja <tkukreja@esri.com>
2022-09-17 21:20:37 -0700

    Time: 48 ms (78.86%), Space: 18.3 MB (55.55%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.py
+++ 110-balanced-binary-tree/110-balanced-binary-tree.py
@@ -13,29 +13,82 @@ class Solution(object):
         if there is no nodes then ? balanced -> true
         if there is 1 node then also -> yes
         
+        for every node we'll have to check whether it is balanced or not
+        
+        space complexity -> 
+        time complexity -> 
+        
+        if there are n nodes in a balanced binary tree, its height is 
+        logn -> 1
+        
+        we stop as soon as we see the tree is not balanced
+        
+        while checking for every node for balanced subtrees, we'll only reach the leaf node if their parent is balanced tree
+        
+        in a balanced tree on every level the time complexity for checking balanced tree will be O(n) and then using 1, the total time complexity is n X logn = O(n logn)
+        
+        
+                o    n          
+                /\
+                o o  (n/2 + n/2)
+                   .
+                   .                   height = logn
+                   .
+                   
+                   
+        
+        
+        bottom-up approach
+        https://www.youtube.com/watch?v=QfJsau0ItOY
+        start from the leaf node, check if it is balanced and return [Bool, height] to parent
+        
+        if Bool is False, keep returning False to curr node's parent
+        
+        if Bool is True from both the left subtree and right subtree then check their height and return max(leftSubtreeHeight, rightSubtreeHeight) + 1 for the current node and True to its parent
+        
+        since every node will only be visited only twice, once while going down the tree, and other time while coming up, hence the time complexity = O(n)
+        
+        space complexity -> if the tree is skewed then the worst case space  will be O(n), else, if it is balanced then O(logn), basically 
+        O(h)
         """
         
-        def findDepth(node):
-            if node is None:
-                return 0
-            
-            leftSubtreeHeight = findDepth(node.left)
-            rightSubtreeHeight = findDepth(node.right)
-            
-            return max(leftSubtreeHeight, rightSubtreeHeight) + 1
+        # O(n) time complexity -> bottom-up approach
         
-        def isBalancedBinaryTree(node):
-            if node is None:
-                return True
-            leftSubtreeHeight = findDepth(node.left)
-            rightSubtreeHeight = findDepth(node.right)
+        def traverseDownTheTree(node):
+            if not node:
+                return [True, 0]
+            leftSubtree =traverseDownTheTree(node.left)
+            rightSubtree = traverseDownTheTree(node.right)
             
-            if (leftSubtreeHeight > rightSubtreeHeight + 1) or (rightSubtreeHeight > leftSubtreeHeight + 1):
-                return False
-            else:
-                return (isBalancedBinaryTree(node.left)) and (isBalancedBinaryTree(node.right))
+            balanced = (leftSubtree[0] and rightSubtree[0] and abs(leftSubtree[1] - rightSubtree[1]) <= 1)
+            
+            return [balanced, max(leftSubtree[1], rightSubtree[1]) + 1]
+        return traverseDownTheTree(root)[0]
+            
+            
         
-        return isBalancedBinaryTree(root)
+#         # O(nlogn) -> top -down approach
+#         def findDepth(node):
+#             if node is None:
+#                 return 0
+            
+#             leftSubtreeHeight = findDepth(node.left)
+#             rightSubtreeHeight = findDepth(node.right)
+            
+#             return max(leftSubtreeHeight, rightSubtreeHeight) + 1
+        
+#         def isBalancedBinaryTree(node):
+#             if node is None:
+#                 return True
+#             leftSubtreeHeight = findDepth(node.left)
+#             rightSubtreeHeight = findDepth(node.right)
+            
+#             if (leftSubtreeHeight > rightSubtreeHeight + 1) or (rightSubtreeHeight > leftSubtreeHeight + 1):
+#                 return False
+#             else:
+#                 return (isBalancedBinaryTree(node.left)) and (isBalancedBinaryTree(node.right))
+        
+#         return isBalancedBinaryTree(root)
             
             
                             
